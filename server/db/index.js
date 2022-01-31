c7f7795b315190275229ab32d40bb03e788df8a2
Jill Sherman <jillsherman@MacBook-Pro.local>
2022-01-31 16:42:00 -0500

    associations created

--- server/db/index.js
+++ server/db/index.js
@@ -1,14 +1,17 @@
 //this is the access point for all things database related!
 
-const db = require('./db')
+const db = require('./db');
 
-const User = require('./models/User')
+const User = require('./models/User');
+const Shop = require('./models/Shop');
 
 //associations could go here!
+User.belongsToMany(User);
+User.hasOne(Shop);
 
 module.exports = {
   db,
   models: {
     User,
   },
-}
+};
