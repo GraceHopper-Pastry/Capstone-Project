b7fcc1225f4e899b4b1f836576ad74abf81203c5
China Hoffman <china.hoffman@gmail.com>
2022-02-01 23:01:29 -0500

    successfully seeded database with users

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,12 @@
 //this is the access point for all things database related!
 
-const db = require('./db');
+const db = require("./db");
 
-const User = require('./models/User');
-const Shop = require('./models/Shop');
+const User = require("./models/User");
+const Shop = require("./models/Shop");
 
 //associations could go here!
-User.belongsToMany(User as "Mentor");
+User.hasMany(User, { foreignKey: "mentorId" });
 User.hasOne(Shop);
 
 module.exports = {
