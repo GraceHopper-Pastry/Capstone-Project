08c17ffb9359c2dd307e8c3bb784091fb67187ff
Jill Sherman <jillsherman@MacBook-Pro.local>
2022-02-03 15:41:09 -0500

    home page links to userprofile

--- client/components/Home.js
+++ client/components/Home.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
 import reactRouterDom from 'react-router-dom';
 import ExperienceDialog from './experiencePopup';
 /**
@@ -22,11 +24,15 @@ class Home extends React.Component {
   }
 
   render() {
-    console.log();
+    console.log(this.props);
     return (
       <div>
         {/* <ExperienceDialog /> */}
         <h3>Welcome, {this.props.firstName}</h3>
+
+        <Link to={`/users/${this.props.id}`}>
+          <p>View Profile</p>
+        </Link>
       </div>
     );
   }
@@ -37,6 +43,7 @@ class Home extends React.Component {
 const mapState = (state) => {
   return {
     firstName: state.auth.firstName,
+    id: state.auth.id,
   };
 };
 
