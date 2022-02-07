4659d420e3415a760560d9406e29295fb2e83f19
kayda <kejda.b.celaj@gmail.com>
2022-02-07 11:50:55 -0500

    fix: updated Enzyme

--- client/components/Home.spec.js
+++ client/components/Home.spec.js
@@ -3,7 +3,7 @@
 import {expect} from 'chai'
 import React from 'react'
 import enzyme, {shallow} from 'enzyme'
-import Adapter from 'enzyme-adapter-react-16'
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
 import { Home } from './Home'
 
 const adapter = new Adapter()
