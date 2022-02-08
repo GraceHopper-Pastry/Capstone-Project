5cd4d7726196270eaf30e3567a5a70054efd459a
kayda <kejda.b.celaj@gmail.com>
2022-02-07 23:54:20 -0500

    fix: linkedin auth

--- server/auth/linkedIn.js
+++ server/auth/linkedIn.js
@@ -5,26 +5,46 @@ const { models: { User }} = require('../db')
 require('dotenv').config()
 module.exports = router
 
+var TOKEN = "token";
+if (!process.env.GOOGLE_CLIENT_ID || !process.env.GOOGLE_CLIENT_SECRET) {
+  console.log('Google client ID / secret not found. Skipping Google OAuth.')
+} else {
 const linkedinConfig = {
   clientID: process.env.LINKEDIN_CLIENT_ID,
   clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
-  callbackURL: "http://localhost:8080/auth/linkedin/callback"
+  callbackURL: "http://localhost:8080/auth/linkedin/callback",
+  scope: ['r_emailaddress', 'r_liteprofile'],
+  profileFields: ['email-address'],    
+  state: true 
 }
 
 passport.use(new LinkedInStrategy(linkedinConfig, (token, refreshToken, profile, done) => {
-  const linkedinId = profile.id
-  const name = profile.displayName
+  console.log("profile:" +profile)
   const email = profile.emails[0].value
-  return User.find({where: {linkedinId}})
-    .then(user => user
-      ? done(null, user)
-      : User.create({name, email, linkedinId})
-        .then(user => done(null, user))
-    )
-    .catch(done)
-}))
-router.get('/', passport.authenticate('linkedin', {scope: 'email'}))
-router.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-  successRedirect: '/home',
-  failureRedirect: '/login'
-}))
+    const password = '123'
+    TOKEN = token;
+
+    User.findOrCreate({
+      where: {email},
+      defaults: {email, password}
+    })
+      .then(([user]) => {
+        const jwt  = user.generateToken();
+        console.log("generated token")
+        console.log(token)
+        done(null, {user, token: jwt});
+      });
+  }
+))
+router.get('/', passport.authenticate('linkedin'))
+router.get('/callback', passport.authenticate('linkedin'), (req, res)=>{
+  try {
+    res.cookie("token", req.user.token);
+    res.redirect('/home');
+    
+  } catch (err) {
+    console.log("error:");
+    console.log(err)
+  }
+})
+}
