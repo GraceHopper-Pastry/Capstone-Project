9f398ec341d97fb34166c88cb7391412a1f6f789
Jill Sherman <jillsherman@MacBook-Pro.local>
2022-02-08 13:47:24 -0500

    matches in redux store

---
+++ client/store/matches.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+// matches action type
+const GET_MATCHES = 'GET_MATCHES';
+
+// matches action creator
+export const getMatches = (matches) => ({
+  type: GET_MATCHES,
+  matches,
+});
+
+// fetchMatches Thunk to get matches
+export const fetchMatches = () => {
+  return async (dispatch) => {
+    try {
+      // aliasing data as the variable name 'matches'
+      const { data: matches } = await axios.get('/api/users');
+      dispatch(getMatches(matches));
+    } catch (err) {
+      console.log('fetchMatches thunk error!!', err);
+    }
+  };
+};
+
+const initialState = [];
+
+export default function matchesReducer(state = initialState, action) {
+  switch (action.type) {
+    case GET_MATCHES:
+      return action.matches;
+    default:
+      return state;
+  }
+}
