b227760e07fa29533406fd776199f81e5ca18e7e
Carly Sandler <carly.sandler@gmail.com>
2022-02-14 22:03:42 -0500

    tech stack

--- client/logged_out/components/landing_page/TechStack.js
+++ client/logged_out/components/landing_page/TechStack.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import SwipeableViews from 'react-swipeable-views';
-
+import { makeStyles } from "@mui/styles";
+import { Typography, Image } from "@mui/material"
 const customPalette = {
   matisse: '#16738D',
   pictonBlue: '#24C0EB',
@@ -16,58 +17,105 @@ const customPalette = {
   black: '#24282C',
   white: '#FFFF',
 }
+const useStyles = makeStyles(() => {
+  const baseStyle = {
+    lineHeight: "300px",
+    color: "black"
+  };
 
-const styles = (theme) =>({
-  slide: {
-    padding: 15,
-    minHeight: 100,
-    color: '#fff',
-  },
-  slide1: {
-    background: customPalette[0],
-  },
-  slide2: {
-    background: customPalette[1],
-  },
-  slide3: {
-    background: customPalette[2],
-  },
-  slide4: {
-    background: customPalette[3],
-  },
-  slide5: {
-    background: customPalette[4],
+  return {
+    slide0: {
+      ...baseStyle,
+      backgroundColor: customPalette[0],
+    },
+    slide1: {
+      ...baseStyle,
+      backgroundColor: customPalette[1],
+    },
+    slide2: {
+      ...baseStyle,
+      backgroundColor: customPalette[2],
+    },
+    slide3: {
+      ...baseStyle,
+      backgroundColor: customPalette[3],
+    },
+    slide4: {
+      ...baseStyle,
+      backgroundColor: customPalette[4],
+    },
+    slide5: {
+      ...baseStyle,
+      backgroundColor: customPalette[5],
+    }
   }
-});
 
+})
 
-const TechStack = () => (
-  <SwipeableViews enableMouseEvents>
-    <div className="stackPostgres" style={Object.assign({}, styles.slide, styles.slide1)}>
-      Postgresql
+const stack = [
+  {
+    name: "Postgresql",
+    src: "./images/stack/postgres.jpeg"
+  },
+  {
+    name: "Sequelize",
+    src: "./images/stack/sequelize.png"
+  },
+  {
+    name: "Express",
+    src: "./images/stack/express.png"
+  },
+  {
+    name: "React",
+    src: "./images/stack/react.png"
+  },
+  {
+    name: "Redux",
+    src: "./images/stack/redux.png"
+  },
+  {
+    name: "Node.js",
+    src: "./images/stack/node.png"
+  },
+  {
+    name: "MUI Material",
+    src: "./images/stack/mui.png"
+  },
+  {
+    name: "Socket.io",
+    src: "./images/stack/socketio.png"
+  }
+];
+
+const TechStack = () => {
+  const [swipeableActions, setSwipeableActions] = React.useState();
+  const classes = useStyles();
+
+  return (
+    <div>
+      <h1>Our Tech Stack</h1>
+      <div className="left-right" style={{backgroundColor: customPalette.concrete}}>
+      <SwipeableViews
+      enableMouseEvents
+      action={actions => setSwipeableActions(actions)}
+          resistance
+          animateHeight
+        >
+          <div className="dev-card">
+            {stack.map((tech, index) => (
+            <div className={classes[`slide${index}`]} key={tech.name}>
+
+              <h1>{tech.name}</h1>
+              <img
+                className="dev-profile"
+                src={tech.src} />
+              </div>
+          ))}
+          </div>
+        </SwipeableViews>
+      </div>
     </div>
-    <div className="stackSequelize" style={Object.assign({}, styles.slide, styles.slide2)}>
-      Sequelize
-    </div>
-    <div className="stackExpress" style={Object.assign({}, styles.slide, styles.slide3)}>
-      Express
-    </div>
-    <div className="stackReact" style={Object.assign({}, styles.slide, styles.slide3)}>
-      React
-    </div>
-    <div className="stack" style={Object.assign({}, styles.slide, styles.slide3)}>
-      Redux
-    </div>
-    <div className="stack" style={Object.assign({}, styles.slide, styles.slide3)}>
-      Node.js
-    </div>
-    <div className="stack" style={Object.assign({}, styles.slide, styles.slide3)}>
-      MUI Material
-    </div>
-    <div className="stack" style={Object.assign({}, styles.slide, styles.slide3)}>
-      Socket.io
-    </div>
-  </SwipeableViews>
-);
+  );
+}
 
 export default TechStack;
