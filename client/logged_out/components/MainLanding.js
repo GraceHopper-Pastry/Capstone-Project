ea41e96d3646d09a0d6c0d88a2777d76d478a8e3
China Hoffman <china.hoffman@gmail.com>
2022-02-14 22:40:16 -0500

    working on backup landing page

--- client/logged_out/components/MainLanding.js
+++ client/logged_out/components/MainLanding.js
@@ -1,11 +1,13 @@
 import React, { memo, Fragment } from "react";
 import PropTypes from "prop-types";
-import { withStyles } from '@mui/styles';
-import HeaderSection from './landing_page/HeaderSection'
-import OfferSection from './landing_page/OfferSection'
-import Footer from './footer/Footer';
-import CookieConsent, { Cookies, getCookieConsentValue } from "react-cookie-consent";
-
+import { withStyles } from "@mui/styles";
+import HeaderSection from "./landing_page/HeaderSection";
+import OfferSection from "./landing_page/OfferSection";
+import Footer from "./footer/Footer";
+import CookieConsent, {
+  Cookies,
+  getCookieConsentValue,
+} from "react-cookie-consent";
 
 const styles = (theme) => ({
   wrapper: {
@@ -14,24 +16,21 @@ const styles = (theme) => ({
   },
 });
 
-const MainLanding = props => {
+const MainLanding = (props) => {
   const { classes } = props;
-  console.log(getCookieConsentValue());
+
   return (
     <div className={classes.wrapper}>
       <Fragment>
         <HeaderSection />
-        {/* <OfferSection /> */}
         <Footer />
       </Fragment>
     </div>
-  )
-}
+  );
+};
 
 MainLanding.propTypes = {
   classes: PropTypes.object.isRequired,
 };
 
 export default withStyles(styles, { withTheme: true })(memo(MainLanding));
-
-
